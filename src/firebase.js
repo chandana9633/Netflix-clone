627db6445f382e5bb2e7bbdfb34df750a172a085
chandana9633 <chandana8113pdas@gmail.com>
2026-05-28 11:19:59 +0530

    Moved Firebase config to env

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
-
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBl-t9gvGZVXbhVFm6tqJUz5d8NXEJhQbg",
-  authDomain: "netflix-clone-155d3.firebaseapp.com",
-  projectId: "netflix-clone-155d3",
-  storageBucket: "netflix-clone-155d3.firebasestorage.app",
-  messagingSenderId: "618800491017",
-  appId: "1:618800491017:web:4a0cfad50f67e71da5a6d8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+
+export const auth = getAuth(app);
